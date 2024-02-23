c21bf075ba9012807dc2a907c449bc677aaebe9e
IT21221064 <99318000+IT21221064@users.noreply.github.com>
2024-02-23 12:27:09 +0530

    done changes

--- EmployeeManagementAPI/EmployeeManagementAPI/Models/Employee.cs
+++ EmployeeManagementAPI/EmployeeManagementAPI/Models/Employee.cs
@@ -12,6 +12,7 @@ namespace EmployeeManagementAPI.Models
         public string? MobileNumber { get; set; } // Assuming mobile number can be a string
         public string? Email { get; set; }
         public string Birthday { get; set; }
+        public string? DateOfJoining { get; set; }
         public List<Department> Departments { get; set; } // Collection of departments
 
         // Constructor for setting default values
