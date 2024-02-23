8afb68bdd134e90703249ef4303c94e306800906
IT21221064 <99318000+IT21221064@users.noreply.github.com>
2024-02-23 12:02:10 +0530

    adding main files

---
+++ EmployeeManagementAPI/EmployeeManagementAPI/Controllers/DepartmentController.cs
@@ -0,0 +1,66 @@
+﻿using EmployeeManagementAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace EmployeeManagementAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public DepartmentController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpGet]
+        public JsonResult Get() {
+            string query = @"select DeptId, DeptName from dbo.Department";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("DefaultConnection");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+
+        [HttpPost]
+        public JsonResult Post(Department dept) {
+            string query = @"insert into dbo.Department values (@DeptName)";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("DefaultConnection");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@DeptName", dept.DeptName);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Added Successfully");
+
+        }
+
+
+
+    }
+}
+
---
+++ EmployeeManagementAPI/EmployeeManagementAPI/Models/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EmployeeManagementAPI.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Address { get; set; }
+        public string? MobileNumber { get; set; } // Assuming mobile number can be a string
+        public string? Email { get; set; }
+        public DateTime Birthday { get; set; }
+        public List<Department> Departments { get; set; } // Collection of departments
+
+        // Constructor for setting default values
+        public Employee()
+        {
+            Birthday = DateTime.MinValue; // or any other default value you prefer
+            Departments = new List<Department>();
+        }
+    }
+
+}
