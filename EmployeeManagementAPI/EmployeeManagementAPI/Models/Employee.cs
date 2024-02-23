afc2c2e993607a2df13eace0e59b9280dbd8fd5b
IT21221064 <99318000+IT21221064@users.noreply.github.com>
2024-02-23 12:25:32 +0530

    changes done

--- EmployeeManagementAPI/EmployeeManagementAPI/Models/Employee.cs
+++ EmployeeManagementAPI/EmployeeManagementAPI/Models/Employee.cs
@@ -11,13 +11,12 @@ namespace EmployeeManagementAPI.Models
         public string? Address { get; set; }
         public string? MobileNumber { get; set; } // Assuming mobile number can be a string
         public string? Email { get; set; }
-        public DateTime Birthday { get; set; }
+        public string Birthday { get; set; }
         public List<Department> Departments { get; set; } // Collection of departments
 
         // Constructor for setting default values
         public Employee()
         {
-            Birthday = DateTime.MinValue; // or any other default value you prefer
             Departments = new List<Department>();
         }
     }
